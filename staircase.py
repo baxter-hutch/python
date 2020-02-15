37e513b63b749dd88d483c5a7086ddf5fd0652aa
baxter-hutch <baxtech@gmail.com>
2020-02-14 20:43:44 -0800

    staircase.py
    test

---
+++ staircase.py
@@ -0,0 +1,25 @@
+import math
+import os
+import random
+import re
+import sys
+
+# Complete the staircase function below.
+def staircase(n):
+    cor_x = 0
+    cor_y = 0 #cor_y = 1: top
+    while cor_y < n:
+        while cor_x < n:
+            if(cor_x < (n - cor_y - 1)):
+                print(" ", end='')
+            else:
+                print("#", end='')
+            cor_x+=1
+        cor_x = 0
+        print('')
+        cor_y+=1
+
+if __name__ == '__main__':
+    n = 6
+
+    staircase(n)
